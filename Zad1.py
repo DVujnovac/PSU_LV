cd8dd287ce0c70dbc8eeaa9f80f28f90d175a637
DVujnovac <vujnovacdavid03@gmail.com>
2025-07-01 14:39:30 +0200

    Delete Zad1.py

--- Zad1.py
+++
@@ -1,9 +0,0 @@
-def umnozak(radni, euri):
-    return radni*euri
-
-radni = float(input("Unesite radne sate: "))
-euri = float(input("Unesite koliko je plaćen sat: "))
-
-ukupno = umnozak(radni, euri)
-
-print("Ukupno: ", ukupno, " eura")
